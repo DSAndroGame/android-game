9e397219717232fc5282c45a5c890edc09da139d
luissanchez1908 <luissanchez1908@gmail.com>
2018-10-18 00:46:57 -0400

    Update GameView.java

--- game1/GameView.java
+++ game1/GameView.java
@@ -1,30 +1,55 @@
 package edu.kean.game1;
 
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.view.View;
 import android.graphics.Canvas;
 import android.graphics.Bitmap;
 import android.content.Context;
+import android.graphics.Paint;
 
 
 public class GameView extends View {
 
 
-    //boy
-    private Bitmap zombie;
+    //Variables
+    private Bitmap ghost;
     private Bitmap background;
+
+    //Score
+    private Paint score = new Paint();
+
+    //Level
+    private Paint level = new Paint();
 
     public GameView(Context context){
 
-       super(context);
-        zombie= BitmapFactory.decodeResource(getResources(), R.drawable.zombie);
-        background= BitmapFactory.decodeResource(getResources(), R.drawable.city);
+        super(context);
+        background= BitmapFactory.decodeResource(getResources(), R.drawable.background1);
+        ghost= BitmapFactory.decodeResource(getResources(), R.drawable.ghost);
+
+        //Score set up
+        score.setColor(Color.WHITE);
+        score.setTextSize(50);
+        score.setTypeface(Typeface.DEFAULT_BOLD);
+        score.setAntiAlias(true);
+
+        //Level Set up
+        
+        score.setColor(Color.WHITE);
+        score.setTextSize(50);
+        score.setTypeface(Typeface.DEFAULT_BOLD);
+        score.setAntiAlias(true);
     }
 
 
     protected void onDraw(Canvas canvas){
-        canvas.drawBitmap(background, 0,0,null);
-        canvas.drawBitmap(zombie,0,0, null);
+
+        canvas.drawBitmap(background,0,0, null);
+        canvas.drawBitmap(ghost,0,300, null);
+        canvas.drawText("Coins: 0", 20, 60, score);
+        canvas.drawText("LVL: 0", 800, 60, score);
 
 
     }
