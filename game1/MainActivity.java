a4faeb155c026f5a7acfc356c3d68449bbcd9c44
luissanchez1908 <luissanchez1908@gmail.com>
2018-10-18 09:56:45 -0400

    Update MainActivity.java

--- game1/MainActivity.java
+++ game1/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.os.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     private GameView gameView;
     public Button button1;
+
+    private Handler handler = new Handler();
+    private final static long TIMER_INTERVAL = 60;
 
     //buttum set Up
 //    public void init (){
@@ -39,6 +46,20 @@ public class MainActivity extends AppCompatActivity {
         //init();
         gameView=new GameView(this);
         setContentView(gameView);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask(){
+
+            public void run(){
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        gameView.invalidate();
+                    }
+                });
+
+            }
+        }, 0, TIMER_INTERVAL);
 
 
 
